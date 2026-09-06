e3475a78652217421e92ace0b3eab4a866405a64
munz-cpu <munzhoo12@gmail.com>
2026-09-06 13:43:41 +1000

    일단 json 파일 만듦

---
+++ Assets/Script/글자/장문/장문연습.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class 장문연습 : MonoBehaviour
+{
+    [System.Serializable]
+    public class MyData
+    {
+        public string text;
+    }
+
+    public MyData myData;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
