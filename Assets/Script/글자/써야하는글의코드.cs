2424372358582219942467241804b702868e950e
munz-cpu <munzhoo12@gmail.com>
2026-09-19 02:10:18 +1000

    글 루프 추가

--- Assets/Script/글자/써야하는글의코드.cs
+++ Assets/Script/글자/써야하는글의코드.cs
@@ -1,5 +1,8 @@
+using System.Collections;
 using TMPro;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 //사용법
 //얘는 써야하는 글(canvas 오브젝트)의 글 내용으로 자동으로 판단함
@@ -7,32 +10,37 @@ using UnityEngine;
 
 public class 써야하는글의코드 : MonoBehaviour
 {
-    [SerializeField] TextMeshProUGUI inputText;
+    [SerializeField] bool 크리에이티브장문 = false;
+    [SerializeField] int 몇개씀 = 0;
+    [SerializeField] TMP_InputField 입력글오브젝트;
     [SerializeField] RectTransform 써야할글자Rect;
 
     [SerializeField] int 이동시작글자 = 10;
-
+    [SerializeField] 스탯기록용 stat;
+    [SerializeField] 문장불러오기 문장;
     public int 오타수 = 0;
 
     Vector2 처음위치;
-
+    
     TextMeshProUGUI 써야할글자;
     void Awake()
     {
         써야할글자 = GetComponent<TextMeshProUGUI>();
         써야할글자Rect = GetComponent<RectTransform>();
-
+        if (!문장) 문장 = FindAnyObjectByType<문장불러오기>();
         처음위치 = 써야할글자Rect.anchoredPosition;
     }
 
     // 글자를 쓸 때마다 호출
     public void Refresh()
     {
-        오타수 = -1;
+        오타수 = 0;
         써야할글자.ForceMeshUpdate();
 
-        string 입력글 = inputText.text;
+        string 입력글 = 입력글오브젝트.text;
         string 정답글 = 써야할글자.text;
+
+        //Debug.Log("입력글오브젝트 : "+입력글.Length);
 
         // 아무것도 입력 안 했을 때
         if (입력글.Length == 0)
@@ -60,14 +68,12 @@ public class 써야하는글의코드 : MonoBehaviour
         }
 
         // 현재 입력한 마지막 글자는 노랑
-        int 현재글자 = 검사할길이 - 1;
+        int 현재글자 = 검사할길이-1;
 
         if (현재글자 >= 0)
         {
             if (입력글[현재글자] == 정답글[현재글자])
-                글자색바꾸기(현재글자, Color.white);
-            else
-                글자색바꾸기(현재글자, Color.yellow);
+                글자색바꾸기(현재글자+1, Color.yellow);
         }
 
         써야할글자.UpdateVertexData(
@@ -75,10 +81,20 @@ public class 써야하는글의코드 : MonoBehaviour
         );
 
         위치갱신();
+
+        int 써야할글자길이 = 써야할글자.text.Length;
+
+        if (입력글.Length > 써야할글자길이 || (입력글.Length >= 써야할글자길이) && Keyboard.current.enterKey.isPressed)
+        {
+            춘배야다음글받아오거라();    
+        }
+        //테스트용
+        //입력글오브젝트.text="";
     }
     
     private void 글자색바꾸기(int index, Color32 color)
     {
+        if (써야할글자.text.Length-1 < index) return;
         TMP_CharacterInfo charInfo =
             써야할글자.textInfo.characterInfo[index];
 
@@ -100,7 +116,7 @@ public class 써야하는글의코드 : MonoBehaviour
     public void 위치갱신()
     {
         TMP_TextInfo 글자정보 = 써야할글자.textInfo;
-        int 끝글자 = Mathf.Min(inputText.text.Length, 글자정보.characterCount);
+        int 끝글자 = Mathf.Min(입력글오브젝트.text.Length, 글자정보.characterCount);
         int 시작글자 = Mathf.Clamp(이동시작글자, 0, 글자정보.characterCount);
 
         if (끝글자 <= 시작글자)
@@ -117,5 +133,40 @@ public class 써야하는글의코드 : MonoBehaviour
 
         써야할글자Rect.anchoredPosition =
             처음위치 + Vector2.left * 이동거리;
+    }
+
+    public void 춘배야다음글받아오거라()
+    {
+        춘배야지금쓰는중인거초기화해라();
+        춘배야써야하는글보여주는거초기화해라();
+    }
+
+    public void 춘배야지금쓰는중인거초기화해라()
+    {
+        stat.완료한입력글자수 += 써야할글자.text.Length;
+        stat.전체오타수 += 오타수;
+        오타수 = 0;
+        입력글오브젝트.text = "";
+        몇개씀++;
+        StartCoroutine(다시글쓰기시작());   
+    }
+
+    IEnumerator 다시글쓰기시작()
+    {
+        yield return null;
+        입력글오브젝트.ActivateInputField();
+    }
+    public void 춘배야써야하는글보여주는거초기화해라()
+    {
+        if (크리에이티브장문)
+        {
+            
+        }
+        else
+        {
+            써야할글자.text=문장.랜덤문장불러오기().text;
+        }
+        
+        
     }
 }
--- Assets/Script/글자/쓰고있는글.cs
+++ Assets/Script/글자/쓰고있는글.cs
@@ -1,24 +1,42 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 using TMPro;
 
 public class 쓰고있는글 : MonoBehaviour
 {
     [SerializeField] TMP_InputField inputField;
-
     [SerializeField] 써야하는글의코드 textToWrite;
-
     [SerializeField] 스탯기록용 stat;
 
     void Awake()
     {
         inputField.onValueChanged.AddListener(입력바뀜);
-        if (!stat) stat = FindAnyObjectByType<스탯기록용>();
+
+        if (!stat)
+            stat = FindAnyObjectByType<스탯기록용>();
+    }
+
+    void Update()
+    {
+        if (Keyboard.current.enterKey.wasPressedThisFrame ||
+            Keyboard.current.numpadEnterKey.wasPressedThisFrame)
+        {
+            엔터누름();
+        }
     }
 
     void 입력바뀜(string 현재글)
     {
-        //Debug.Log("글자 바뀜: " + 현재글);
         textToWrite.Refresh();
         stat.입력시작();
+    }
+
+    void 엔터누름()
+    {
+        //Debug.Log("엔터!");
+        
+        inputField.text = inputField.text + " ";
+
+        
     }
 }
